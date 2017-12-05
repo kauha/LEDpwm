99303f84d7d0f1559db1fcb3a3df0b4fc0c3242c
Ville Kauhanen <ville.kauhanen@gmail.com>
2017-12-06 01:07:39 +0200

    Changes

--- ledpwm.js
+++ ledpwm.js
@@ -1,4 +1,17 @@
-var piblaster = require('pi-blaster.js');
+Define piblaster = require('pi-blaster.js'); // Require pi-blaster lib
+const GPIO_PINS = [18, 23, 24];		// Define used GPIO pins
+const STEPS = 255;		// The number of brightness levels
+
+var brightness = new Array(STEPS);
+
+function array_creator(brightness) {
+	var a = 0;
+	while(a <= STEPS) {
+		brightness[a] = (a / STEPS);
+		a++;
+	}
+	console.log(brightness);
+}
 
 piblaster.setPwm(18, 1 ); // 100% brightness
 piblaster.setPwm(23, 0.2 ); // 20% brightness
