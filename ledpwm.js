24db3ca33bffc5464d8a3c39c914ae36755c7eee
Ville Kauhanen <ville.kauhanen@gmail.com>
2017-12-06 00:37:56 +0200

    Changes

--- ledpwm.js
+++ ledpwm.js
@@ -1,5 +1,5 @@
 var piblaster = require('pi-blaster.js');
 
-piblaster.setPwm(18, 1 ); # 100% brightness
-piblaster.setPwm(23, 0.2 ); # 20% brightness
-piblaster.setPwm(24, 0 ); # off
+piblaster.setPwm(18, 1 ); // 100% brightness
+piblaster.setPwm(23, 0.2 ); // 20% brightness
+piblaster.setPwm(24, 0 ); // off
