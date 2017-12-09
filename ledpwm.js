6cd46016fb079476deb9172de198e334c41d396d
Ville Kauhanen <ville.kauhanen@gmail.com>
2017-12-10 01:51:36 +0200

    Fixes

--- ledpwm.js
+++ ledpwm.js
@@ -3,14 +3,15 @@ var sleep = require('sleep'); //Sleeping
 const red_pin = 18;
 const green_pin = 23;
 const blue_pin = 24;
-const GPIO_PINS = [red_pin, green_pin, blue_pin];		// Define used GPIO pins
+const GPIO_PINS = [red_pin, green_pin, blue_pin];
+const COLORS = [0,1,2];		// Define used GPIO pins
 const STEPS = 256;		// The number of brightness levels
-const DIRECTIONS = [-1,1]; 
+
+var current_colors = [0,0,0];
 
 var brightness = new Array(STEPS);
 
-var random_pin = GPIO_PINS[0];
-var random_direction = DIRECTIONS[1];
+var direction = 1
 var a = 0;
 
 function array_creator(brightness) {
@@ -26,31 +27,34 @@ Array.prototype.random = function () {  // Generates random values
 }
 
 function color_looper() {
-	if (random_direction == 1){
+	if (direction == 1){
 		if(a < STEPS) {
 			sleep.msleep(10);
-			color_changer(random_pin, brightness[a], random_direction);
+			color_changer(current_colors[random_color], brightness[a], direction);
 			a++;
 		} else {
-				random_pin = GPIO_PINS.random();
-				random_direction = DIRECTIONS.random();
+				random_color = COLORS.random();
+				if (current_colors[random_color] == 0){
+					direction = 1;
+				} else {
+					direction = -1;
+				}
 				color_looper();	
 		}
 	} else {
 		if(a >= 1) {
 			sleep.msleep(10);
-			color_changer(random_pin, brightness[a], random_direction);
+			color_changer(random_color, brightness[a], direction);
 			a--;
 		} else {
-				random_pin = GPIO_PINS.random();
-				random_direction = DIRECTIONS.random();
+				random_color = COLORS.random();
 				color_looper();
 		}
 	}
 }
 
 function color_changer(pin, brightness_level, direction) {
-	piblaster.setPwm(pin, brightness_level, function(callback) {
+	piblaster.setPwm(GPIO_PINS[pin], brightness_level, function(callback) {
 		console.log(pin, brightness_level, direction);
 		color_looper();
 	});
