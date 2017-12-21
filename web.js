6cca9bc1df62593bca9e2506ed2aff02f46c9262
Ville Kauhanen <ville.kauhanen@gmail.com>
2017-12-21 17:21:03 +0200

    Start of web

--- web.js
+++ web.js
@@ -1,38 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
 
-http.createServer( function (request, response) {
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
-	var pathname = url.parse(request.url).pathname;
-
-	if (pathname == "/") {
-		pathname = "/index.htm";
-	}
-
-	// Print the name of the file for which request is made.
-   console.log("Request for " + pathname + " received.");
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
    
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else {	
-         //Page found	  
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         
-         // Write the content of the file to response body
-         response.write(data.toString());		
-      }
-      // Send the response body 
-      response.end();
-   });   
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+   console.log("Server listening at http://%s:%s", host, port)
+})
