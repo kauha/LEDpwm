77d025e83cd30e0b15e4130159ae4c7ec0632066
Ville Kauhanen <ville.kauhanen@gmail.com>
2017-12-21 20:56:17 +0200

    Start of web

--- web.js
+++ web.js
@@ -2,6 +2,34 @@ var express = require('express');
 var app = express();
 var led = require("./ledpwm.js")
 
+/*/// LED STUFF
+var piblaster = require('pi-blaster.js'); // Require pi-blaster lib
+var gpio = require('rpi-gpio');   // Require rpi-gpio lib for starting ATX PSU
+var sleep = require('sleep'); //Sleeping
+
+const power_pin = 7;  // Pin to start up the ATX PSU  !!Different pin numbering
+
+const red_pin = 23;		// Define used GPIO pins
+const green_pin = 25;
+const blue_pin = 24;
+const STEPS = 256;		// The number of brightness levels
+
+const key_array = ['red', 'green', 'blue'];
+var colors = {  // Stores the pins of the colors and the current brightness of the strip
+	'red': {
+		'pin': red_pin,
+		'current_brightness': 0	
+	},
+	'green':{
+		'pin': green_pin,
+		'current_brightness': 0
+	},
+	'blue': {
+		'pin': blue_pin,
+		'current_brightness': 0
+	}
+};		
+///LED STUFF*/
 
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
